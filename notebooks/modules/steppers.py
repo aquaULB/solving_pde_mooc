dce8d39a25addfc3d0e3055b9e460a12ba2c84ce
Bernard Knaepen <bknaepen@gmail.com>
2025-10-03 10:58:06 +0200

    Full env yaml file for faster install; typos

--- notebooks/modules/steppers.py
+++ notebooks/modules/steppers.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 def euler_step(u, f, dt, *args):
-    """Returns the solution at the next time-step using 
+    """Returns the solution at the next time-step using
     the forward Euler method.
-    
+
     Parameters
     ----------
     u : array of floats
@@ -13,7 +14,7 @@ def euler_step(u, f, dt, *args):
     dt : float
         time-increment.
     args : optional arguments for the rhs function
-    
+
     Returns
     -------
     unp1 : array of floats
@@ -22,10 +23,11 @@ def euler_step(u, f, dt, *args):
     unp1 = u + dt * f(u, *args)
     return unp1
 
+
 def rk4_step(u, f, dt, *args):
-    """Returns the solution at the next time-step using 
+    """Returns the solution at the next time-step using
     the RK4 method. Assumes that f is time independent
-    
+
     Parameters
     ----------
     u : array of floats
@@ -35,25 +37,25 @@ def rk4_step(u, f, dt, *args):
     dt : float
         time-increment.
     args : optional arguments for the rhs function
-    
+
     Returns
     -------
     u_n_plus_1 : array of floats
         approximate solution at the next time step.
     """
-    
-    #substep 1
+
+    # substep 1
     k1 = f(u, *args)
-    
-    #substep 2
-    k2 = f(u + dt/2.*k1, *args)
-    
-    #substep 3
-    k3 = f(u + dt/2.*k2, *args)
-    
-    #substep 4
-    k4 = f(u + k3*dt, *args)
-    
-    unp1 = u + dt/6.*(k1 + 2.0*k2 + 2.0*k3 + k4)
-    
+
+    # substep 2
+    k2 = f(u + dt / 2.0 * k1, *args)
+
+    # substep 3
+    k3 = f(u + dt / 2.0 * k2, *args)
+
+    # substep 4
+    k4 = f(u + k3 * dt, *args)
+
+    unp1 = u + dt / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4)
+
     return unp1
